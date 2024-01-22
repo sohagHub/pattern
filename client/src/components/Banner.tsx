d8fe92a41557e3fea64b50f0b3826dd5dbd28aa7
Shahadat Hossain <shahadat@apple.com>
2024-01-22 11:42:19 -0800

    change mint name

--- client/src/components/Banner.tsx
+++ client/src/components/Banner.tsx
@@ -11,7 +11,7 @@ const Banner = (props: Props) => {
   return (
     <div id="banner" className="bottom-border-content">
       <div className="header">
-        <h2 className="everpresent-content__heading">Mint</h2>
+        <h2 className="everpresent-content__heading">ST Finance</h2>
       </div>
     </div>
   );
