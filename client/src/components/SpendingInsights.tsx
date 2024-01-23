1bb2ce08b84238e89b972b679c575072f175cf55
Shahadat Hossain <shahadat@apple.com>
2024-01-22 19:10:27 -0800

    Update chart with selections

--- client/src/components/SpendingInsights.tsx
+++ client/src/components/SpendingInsights.tsx
@@ -1,8 +1,10 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { currencyFilter, pluralize } from '../util';
 import { CategoriesChart } from '.';
 import { TransactionType } from './types';
+import { Bar } from 'recharts';
+import MonthlyCostChart from './MonthlyCost';
 
 interface Props {
   transactions: TransactionType[];
@@ -14,6 +16,8 @@ interface Categories {
 }
 
 export default function SpendingInsights(props: Props) {
+  const [selectedMonth, setSelectedMonth] = useState<string | null>(null);
+  
   // grab transactions from most recent month and filter out transfers and payments
   const transactions = props.transactions;
   const monthlyTransactions = useMemo(
@@ -22,19 +26,52 @@ export default function SpendingInsights(props: Props) {
         const date = new Date(tx.date);
         const today = new Date();
         const oneMonthAgo = new Date(new Date().setDate(today.getDate() - 30));
+        const month = date.toLocaleString('default', { month: 'short' });
+        const year = date.getFullYear();
+        const monthYear = `${month} ${year}`;
         return (
-          date > oneMonthAgo &&
           tx.category !== 'Payment' &&
           tx.category !== 'Transfer' &&
           tx.category !== 'Interest' &&
-          tx.category !== 'Income' 
+          tx.category !== 'Income' &&
+          (selectedMonth ? monthYear === selectedMonth : date > oneMonthAgo)
         );
       }),
+    [selectedMonth, transactions]
+  );
+
+  const monthlyCosts = useMemo(
+    () =>
+      // calculate monthly total cost by month from transactions
+      // and send as as monthlyCosts: {
+      //   month: string;
+      //   cost: number;
+      // }[]; to MonthlyCostChart
+      transactions.reduce((acc: any[], tx) => {
+        if (
+          tx.category === 'Payment' ||
+          tx.category === 'Transfer' ||
+          tx.category === 'Interest' ||
+          tx.category === 'Income'
+        )
+          return acc;
+        const date = new Date(tx.date);
+        const month = date.toLocaleString('default', { month: 'short' });
+        const year = date.getFullYear();
+        const monthYear = `${month} ${year}`;
+        const index = acc.findIndex(item => item.month === monthYear);
+        if (index === -1) {
+          acc.push({ month: monthYear, cost: tx.amount });
+        } else {
+          acc[index].cost = acc[index].cost + tx.amount;
+        }
+        return acc;
+      }, []),
+
     [transactions]
   );
 
   // create category and name objects from transactions
-
   const categoriesObject = useMemo((): Categories => {
     return monthlyTransactions.reduce((obj: Categories, tx) => {
       tx.category in obj
@@ -60,34 +97,38 @@ export default function SpendingInsights(props: Props) {
       namesArray.push([name, namesObject[name]]);
     }
     namesArray.sort((a: any[], b: any[]) => b[1] - a[1]);
-    namesArray.splice(5); // top 5
+    //namesArray.splice(5); // top 5
     return namesArray;
   }, [namesObject]);
 
   return (
     <div>
       <h2 className="monthlySpendingHeading">Monthly Spending</h2>
-      <h4 className="tableSubHeading">A breakdown of your monthly spending</h4>
-      <div className="monthlySpendingText">{`Monthly breakdown across ${
-        props.numOfItems
-      } bank ${pluralize('account', props.numOfItems)}`}</div>
       <div className="monthlySpendingContainer">
         <div className="userDataBox">
           <CategoriesChart categories={categoriesObject} />
         </div>
         <div className="userDataBox">
           <div className="holdingsList">
-            <h4 className="holdingsHeading">Top 5 Vendors</h4>
+            <h4 className="holdingsHeading">Vendors</h4>
             <div className="spendingInsightData">
               <p className="title">Vendor</p> <p className="title">Amount</p>
               {sortedNames.map((vendor: any[]) => (
                 <>
-                  <p>{vendor[0]}</p>
-                  <p>{currencyFilter(vendor[1])}</p>
+                  <div>{vendor[0]}</div>
+                  <div>{currencyFilter(vendor[1])}</div>
                 </>
               ))}
             </div>
           </div>
+        </div>
+      </div>
+      <div className="monthlySpendingContainer">
+        <div className="userDataBoxBarChart">
+          <MonthlyCostChart
+            monthlyCosts={monthlyCosts}
+            onMonthClick={setSelectedMonth}
+          />
         </div>
       </div>
     </div>
