9a266b1987b55bed3f6535f8cb552ae7f6fc4641
Shahadat Hossain <shahadat@apple.com>
2024-02-25 00:33:07 -0800

    Add support for production environment in linkTokens.js

--- server/routes/linkTokens.js
+++ server/routes/linkTokens.js
@@ -6,6 +6,7 @@ const { asyncWrapper } = require('../middleware');
 
 const express = require('express');
 const plaid = require('../plaid');
+const { prodClient } = require('../plaid');
 const fetch = require('node-fetch');
 const { retrieveItemById } = require('../db/queries');
 const {
@@ -26,12 +27,14 @@ router.post(
     try {
       const { userId, itemId } = req.body;
       let accessToken = null;
+      let isProd = false;
       let products = ['transactions']; // must include transactions in order to receive transactions webhooks
       if (itemId != null) {
         // for the link update mode, include access token and an empty products array
         const itemIdResponse = await retrieveItemById(itemId);
         accessToken = itemIdResponse.plaid_access_token;
         products = [];
+        isProd = itemIdResponse.is_prod;
       }
       
       const linkTokenParams = {
@@ -43,14 +46,19 @@ router.post(
         products,
         country_codes: ['US'],
         language: 'en',
-        webhook: 'https://sohagst.duckdns.org:5001' + '/services/webhook',
+        //webhook: 'https://xxxxx.duckdns.org:5001' + '/services/webhook',
         access_token: accessToken,
       };
       // If user has entered a redirect uri in the .env file
       if (redirect_uri.indexOf('http') === 0) {
         linkTokenParams.redirect_uri = redirect_uri;
       }
-      const createResponse = await plaid.linkTokenCreate(linkTokenParams);
+
+      let plaidClient = plaid;
+      if (isProd) {
+        plaidClient = prodClient;
+      }
+      const createResponse = await plaidClient.linkTokenCreate(linkTokenParams);
       res.json(createResponse.data);
     } catch (err) {
       console.log('error while fetching client token', err.response.data);
