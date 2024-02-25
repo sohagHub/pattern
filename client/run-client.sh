9af4c174f66eefa19b113039b79608e14379906a
Shahadat Hossain <shahadat@apple.com>
2024-02-25 00:37:43 -0800

    Add client and server run scripts

---
+++ client/run-client.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set environment variables
+export CHOKIDAR_USEPOLLING=true
+export PORT=3001
+export REACT_APP_SERVER_PORT=5001
+export REACT_APP_PLAID_ENV=development
+
+# Install dependencies
+echo "Installing dependencies..."
+npm install --force
+
+# Set the port for React applications via .env file
+# echo "PORT=3001" > .env
+# For Node.js applications, ensure the server listens on the correct port in your application code
+
+# Start the application
+echo "Starting the application..."
+npm start
+
+# If you have a specific script for development, replace `npm start` with `npm run your-script-name`
---
+++ server/run-server.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Set environment variables
+export CHOKIDAR_USEPOLLING=true
+export PLAID_CLIENT_ID=your_client_id
+export PLAID_SECRET_SANDBOX=your_plaid_secret_sandbox
+export PLAID_SECRET_DEVELOPMENT=your_plaid_secret_development
+export PLAID_SECRET_PRODUCTION=your_plaid_secret_production
+export PLAID_SANDBOX_REDIRECT_URI=http://localhost:3001/oauth-link
+export PLAID_DEVELOPMENT_REDIRECT_URI=http://localhosts:3001/oauth-link
+export PLAID_ENV=development
+export PORT=5001
+export DB_PORT=5432
+export DB_HOST_NAME=localhost
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=xxxxxx
+# Add any other environment variables following the same pattern
+
+# Install dependencies (if necessary)
+echo "Installing dependencies..."
+npm install
+
+# Optionally, clear out node_modules and reinstall if you're switching environments or need a clean slate
+# rm -rf node_modules
+# npm ci
+
+# Run the server
+echo "Starting the server..."
+npm run debug
+
+# Replace `npm run debug` with the appropriate command to start your server,
+# such as `npm start` for production environments or other custom npm scripts you have defined.
