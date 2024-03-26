b45ebf9c6c1f01d5a43e8d9b3c52c5d51c3dd2b9
Shahadat Hossain <shahadat@apple.com>
2024-03-25 17:17:54 -0700

    Update responsive styles and chart width calculation

--- client/src/components/CategoriesChart.tsx
+++ client/src/components/CategoriesChart.tsx
@@ -10,7 +10,7 @@ interface Props {
 }
 
 export default function CategoriesChart(props: Props) {
-  const widthCalculation = () => window.innerWidth < 800 ? window.innerWidth - 100 : 500;
+  const widthCalculation = () => window.innerWidth < 1000 ? window.innerWidth - 100 : 500;
   const [chartWidth, setChartWidth] = useState(widthCalculation());
 
   useEffect(() => {
